14483b9fdeebe41647270785f32e7f530300cb0f
MMarcel02 <marciniak.marc@gmail.com>
2025-06-08 13:13:43 +0200

    Methods and parameters

--- tmcdata/mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ tmcdata/mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -3,20 +3,47 @@ public class AdvancedAstrology {
 
     public static void printStars(int number) {
         // part 1 of the exercise
+        for (int i = 0; i <= number; i++) {
+            System.out.print("*");
+        }
     }
-
+    
     public static void printSpaces(int number) {
         // part 1 of the exercise
+        for (int i = 0; i < number ; i++) {
+            System.out.print(" ");
+        }
     }
-
+    
     public static void printTriangle(int size) {
         // part 2 of the exercise
+        int starCount = 0;
+        for (int i = size - 1; i >= 0; i--) {
+            printSpaces(i);
+            printStars(starCount);
+            starCount++;
+            System.out.println("");
+        }
     }
 
     public static void christmasTree(int height) {
         // part 3 of the exercise
-    }
+        int starCount = 0;
+        for (int i = height - 1; i >= 0; i--) {
+            printSpaces(i);
+            printStars(starCount);
+            starCount += 2;
+            System.out.println("");
+        }
+        printSpaces(height-2);
+        printStars(2);
+        System.out.println("");
 
+        printSpaces(height-2);
+        printStars(2);
+        System.out.println("");
+
+    }
     public static void main(String[] args) {
         // The tests are not checking the main, so you can modify it freely.
 
@@ -24,6 +51,6 @@ public class AdvancedAstrology {
         System.out.println("---");
         christmasTree(4);
         System.out.println("---");
-        christmasTree(10);
+        // christmasTree(10);
     }
 }
