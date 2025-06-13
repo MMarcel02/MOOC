fc62f138d98c0c89459c5884f9949b68eda213ec
MMarcel02 <marciniak.marc@gmail.com>
2025-06-13 21:34:54 +0200

    methods in an object

--- tmcdata/mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ tmcdata/mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -12,5 +12,25 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+        Statistics allNumbers = new Statistics();
+        Statistics evenNumbers = new Statistics();
+        Statistics oddNumbers = new Statistics();
+        System.out.println("Enter numbers:");
+        while (true) {
+            int value = Integer.valueOf(scanner.nextLine());
+            if (value == -1) {
+                break;
+            }
+            allNumbers.addNumber(value);
+            if (value % 2 == 0) {
+                evenNumbers.addNumber(value);
+            } else {
+                oddNumbers.addNumber(value);
+            }
+        }
+        System.out.println("Sum: " + allNumbers.sum());
+        System.out.println("Sum of even numbers: " + evenNumbers.sum());
+        System.out.println("Sum of odd numbers: " + oddNumbers.sum());
+
     }
 }
