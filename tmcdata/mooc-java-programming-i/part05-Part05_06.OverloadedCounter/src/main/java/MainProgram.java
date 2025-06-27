d294879a9e9e11fcd04505c6564da7bd3969dc7c
MMarcel02 <marciniak.marc@gmail.com>
2025-06-27 11:35:41 +0200

    overloading methods and constructors

--- tmcdata/mooc-java-programming-i/part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
+++ tmcdata/mooc-java-programming-i/part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -3,5 +3,11 @@ public class MainProgram {
 
     public static void main(String[] args) {
         // Test your counter here
+        Counter c = new Counter(5);
+        c.increase();
+        System.out.println(c.value());
+        c.increase(5);
+        System.out.println(c.value());
+
     }
 }
