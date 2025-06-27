c9286126eaeca396b2e1e1fd5f12e52f2a701023
MMarcel02 <marciniak.marc@gmail.com>
2025-06-27 13:51:58 +0200

    comparing equality of objects

--- tmcdata/mooc-java-programming-i/part05-Part05_12.Song/src/main/java/Main.java
+++ tmcdata/mooc-java-programming-i/part05-Part05_12.Song/src/main/java/Main.java
@@ -5,14 +5,13 @@ public class Main {
         // you can write code here for testing your program
 
         Song jackSparrow = new Song("The Lonely Island", "Jack Sparrow", 196);
-        Song anotherSparrow = new Song("The Lonely Island", "Jack Sparrow", 196);
+        Song anotherSparrow = new Song("The Lonely Idsland", "Jack Sparrow", 196);
 
         if (jackSparrow.equals(anotherSparrow)) {
-            System.out.println("Songs are equal.");
-        }
-
-        if (jackSparrow.equals("Another object")) {
+            System.out.println("Same!");
+        } else {
             System.out.println("Strange things are afoot.");
         }
+
     }
 }
--- tmcdata/mooc-java-programming-i/part05-Part05_12.Song/src/main/java/Song.java
+++ tmcdata/mooc-java-programming-i/part05-Part05_12.Song/src/main/java/Song.java
@@ -11,6 +11,26 @@ public class Song {
         this.durationInSeconds = durationInSeconds;
     }
 
+    public boolean equals(Song song) {
+        if (this == song) {
+            return true;
+        }
+
+        if (!(song instanceof Song)) {
+            return false;
+        }
+
+        Song comparedSong = (Song) song;
+        if (name.equals(comparedSong.name) &&
+            durationInSeconds == comparedSong.durationInSeconds &&
+            artist.equals(comparedSong.artist)) {
+            return true;
+        }
+
+        return false;
+
+    }
+
     @Override
     public String toString() {
         return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
